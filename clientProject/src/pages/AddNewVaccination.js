1acc001792249dcc492b8e8392d8b669316a61f0
zisil <zisilevi@gmail.com>
2024-04-02 08:45:30 +0300

    add abc

--- clientProject/src/pages/AddNewVaccination.js
+++ clientProject/src/pages/AddNewVaccination.js
@@ -1,7 +1,7 @@
 // import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 // import DatePicker from 'react-datepicker';
-
+// abc
 // const AddNewVaccination = () => {
 //   const [isAdding, setIsAdding] = useState(false);
 //   const [errorMessage, setErrorMessage] = useState('');
